f98f9969274d7061ac50440f63cebd0a08257017
masonweld <66655954+masonweld@users.noreply.github.com>
2020-06-20 15:15:12 -0500

    Create New_Companies.sql

---
+++ HackerRank_SQL/Advanced_Select/New_Companies.sql
@@ -0,0 +1,12 @@
+--https://www.hackerrank.com/challenges/the-company/problem
+
+SELECT Company.company_code, 
+       Company.founder,
+       COUNT(DISTINCT(Employee.lead_manager_code)),
+       COUNT(DISTINCT(Employee.senior_manager_code)),
+       COUNT(DISTINCT(Employee.manager_code)),
+       COUNT(DISTINCT(Employee.employee_code))
+FROM Company
+INNER JOIN Employee ON (Company.company_code = Employee.company_code)
+GROUP BY Company.company_code, Company.founder
+ORDER BY Company.company_code
